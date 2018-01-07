227383d886bc05e400a287c701ac52b30b586709
Adam Sales <asales@utexas.edu>
2018-01-07 16:33:59 -0600

    hardSecs

--- hardSections.r
+++ hardSections.r
@@ -83,6 +83,8 @@ advance$version <- tolower(advance$version)
 
 x <- x[x$field_id%in%advance$field_id,]
 
+x$ts1 <- as.POSIXct(x$ts1,format="%m/%d/%y %H:%M")
+
 Mode <- function(x){
     tab <- table(x)
     names(tab)[which.max(tab)]
@@ -95,12 +97,18 @@ errHintSum <- x%>%group_by(field_id,version,section,unit)%>%summarize(
                                                                 nhint=sum(nhints1>0,na.rm=TRUE),
                                                                 phint=mean(nhints1>0,na.rm=TRUE),
                                                                 ncurr=n_distinct(curriculum),
-                                                                curr=Mode(curriculum))
+    curr=Mode(curriculum),
+    start= min(ts1,na.rm=TRUE)
+    )
 
 adv2 <- merge(advance,errHintSum,all.x=TRUE)
 
-keep1 <- adv2%>%filter(nerr>0|nhint>0)
-sec100 <- keep1%>%group_by(section)%>%summarize(nn=n())
-adv <- adv2%>%filter((nerr>0|nhint>0) & section%in%sec100$section[sec100$nn>=100])
+adv2 <- adv2%>%arrange(start)%>%group_by(field_id)%>%mutate(secOrd=rank(start))
 
-save(adv,file='advanceHard.RData')
+##keep1 <- adv2%>%filter(nerr>0|nhint>0)
+keep1 <- adv2%>%filter(nhint>0)
+
+sec100 <- keep1%>%group_by(section)%>%summarize(nn=n())
+adv <- adv2%>%filter((nhint>0) & section%in%sec100$section[sec100$nn>=100])
+
+save(adv,file='../advanceHardHint.RData')
